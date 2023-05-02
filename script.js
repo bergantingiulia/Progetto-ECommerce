042b8b668f05bb0ecdc6255df03d28d0b9210e69
bergantingiulia <107368164+bergantingiulia@users.noreply.github.com>
2023-05-02 15:24:25 +0200

    3 files modified

--- script.js
+++ script.js
@@ -18,15 +18,20 @@ function svuotaCarrello() {
 
 
 
+/*swiper*/
 var swiper = new Swiper(".mySwiper", {
-  cssMode: true,
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
-  pagination: {
-    el: ".swiper-pagination",
-  },
-  mousewheel: true,
-  keyboard: true,
-});
+},)
